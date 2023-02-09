1e6120d4beec88a07648dac9d9a24c4268e58fce
tylerkwallace <tylerkwallace@gmail.com>
2023-02-09 12:05:02 -0500

    Add files via upload

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,14 +1,11 @@
 import React from "react";
 import { Rate } from "./Rate";
-import { Thanks } from "./Thanks";
 
 
 export const Card = () => {
     return (
         <div>
             <Rate />
-            <br></br>
-            <Thanks />
         </div>
     )
 
